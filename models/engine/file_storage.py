7fcfbf8413d3cbc253b5e08d70b052c894619079
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-23 16:04:53 +0200

    DecodeError fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,116 +19,120 @@ from models.review import Review
 
 
 class FileStorage:
+    """
+    serializes instances to a JSON file
+    and deserializes JSON file back to instances
+    """
+
+    __file_path, __objects = "file.json", {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
         """
-        serializes instances to a JSON file
-        and deserializes JSON file back to instances
+        Adds a new objects to __objects dictionary.
+
+        Args:
+            obj: The object to be added.
         """
 
-        __file_path, __objects = "file.json", {}
+        special_key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[special_key] = obj
 
-        def all(self):
-            """returns the dictionary __objects"""
-            return FileStorage.__objects
-        def new(self, obj):
-            """
-            Adds a new objects to __objects dictionary.
+    def save(self):
+        """
+        Saves __objects dictionary to a JSON file.
+        """
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as json_f:
+            my_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(my_dict, json_f)
 
-            Args:
-                obj: The object to be added.
-            """
-            special_key = f"{obj.__class__.__name__}.{obj.id}"
-            FileStorage.__objects[special_key] = obj
-        def save(self):
-            """
-            Saves __objects dictionary to a JSON file.
-            """
-            with open(FileStorage.__file_path, "w", encoding="utf-8") as json_f:
-                my_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-                json.dump(my_dict, json_f, indent=4)
+    def reload(self):
+        """
+        Loads objects from JSON file into __objects dictionary.
+        """
+        if not os.path.exists(FileStorage.__file_path):
+            return
+    
+        with open(FileStorage.__file_path, encoding="utf-8") as json_f:
+            serialized_data = json.load(json_f)
+        objects = {}
+        for id, data in serialized_data.items():
+            class_name = data["__class__"]
+            obj_class = self.all_classes()[class_name]
+            objects[id] = obj_class(**data)
+            FileStorage.__objects = objects
 
-        def reload(self):
-            """
-            Loads objects from JSON file into __objects dictionary.
-            """
-            if not os.path.exists(FileStorage.__file_path):
-                return
-            try:
-                    with open(FileStorage.__file_path, "r", encoding="utf-8") as json_file:
-                        serialized_data = json.load(json_file)
-                    objects = {}
-                    for id, data in serialized_data.items():
-                        class_name = data["__class__"]
-                        obj_class = self.all_classes()[class_name]
-                        objects[id] = obj_class(**data)
-                    FileStorage.__objects = objects
-            except json.JSONDecodeError:
-                 pass
+    def all_classes(self):
+        from models.base_model import BaseModel
+        """
+        Helper method
+        Returns a dictionary mapping class names to their actual class objects.
+        """
+        return{
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
 
-        def all_classes(self):
-            from models.base_model import BaseModel
-            """
-            Helper method
-            Returns a dictionary mapping class names to their actual class objects.
-            """
-            return{
-                "BaseModel": BaseModel,
-                "User": User,
-                "State": State,
-                "City": City,
-                "Amenity": Amenity,
-                "Place": Place,
-                "Review": Review
+    def obj_attr(self):
+        """
+        Helper Method
+        Returns a nested dictionary  of class names with the dictionary
+        of their attributes mapping to their types accordingly.
+        """
+        return
+    {
+            "BaseModel":
+            {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            },
+            "User":
+            {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str
+            },
+            "Place":
+            {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": []
+            },
+            "State":
+            {
+                "name": str
+            },
+            "City":
+            {
+                "state_id": str,
+                "name": str
+            },
+            "Review":
+            {
+                "place_id": str,
+                "user_id": str,
+                "text": str
+            },
+            "Amenity":
+            {
+                "name": str
             }
-        def obj_attr(self):
-            """
-            Helper Method
-            Returns all cls attributes mapping to their corresponding types
-            """
-            return{
-                "BaseModel":
-                {
-                    "id": str,
-                    "created_at": datetime.datetime,
-                    "updated_at": datetime.datetime
-                },
-                "User":
-                {
-                    "email": str,
-                    "password": str, 
-                    "first_name": str,
-                    "last_name": str
-                },
-                "Place":
-                {
-                    "city_id": str,
-                    "user_id": str,
-                    "name": str,
-                    "description": str,
-                    "number_rooms": int,
-                    "number_bathrooms": int,
-                    "max_guest": int,
-                    "price_by_night": int,
-                    "latitude": float,
-                    "longitude": float,
-                    "amenity_ids": []
-                },
-                "State":
-                {
-                    "name": str
-                },
-                "City":
-                {
-                    "state_id": str,
-                    "name": str
-                },
-                "Review":
-                {
-                    "place_id": str,
-                    "user_id": str,
-                    "text": str 
-                },
-                "Amenity":
-                {
-                    "name": str
-                }
             }
