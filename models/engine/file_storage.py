377f9b362a952535e0c3e51d289872870700c1e9
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-23 20:44:31 +0200

    open file in read mode by default

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,7 +56,7 @@ class FileStorage:
         if not os.path.exists(self.__file_path):
             return
 
-        with open(FileStorage.__file_path,"r" encoding="utf-8") as json_f:
+        with open(FileStorage.__file_path,"r", encoding="utf-8") as json_f:
             serialized_data = json.load(json_f)
         objects = {}
         for id, data in serialized_data.items():
