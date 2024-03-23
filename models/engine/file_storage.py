4b545136ad45985f527bdc0e4293deb116c7acc4
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-23 20:47:11 +0200

    requirement fail

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,14 +56,14 @@ class FileStorage:
         if not os.path.exists(self.__file_path):
             return
 
-        with open(FileStorage.__file_path,"r", encoding="utf-8") as json_f:
+        with open(FileStorage.__file_path, encoding="utf-8") as json_f:
             serialized_data = json.load(json_f)
         objects = {}
         for id, data in serialized_data.items():
             class_name = data["__class__"]
             obj_class = self.all_classes()[class_name]
             objects[id] = obj_class(**data)
-            FileStorage.__objects = objects
+        FileStorage.__objects = objects
 
     def all_classes(self):
         """
