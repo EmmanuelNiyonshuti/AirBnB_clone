5f4e1f281ff41dde3cebf6e8253ad5eff70a41eb
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-24 20:30:31 +0200

    check fail

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,7 +15,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
-class FileStorage(BaseModel):
+class FileStorage:
     """
     This class performs serialization and deserialization of Python instances.
     """
