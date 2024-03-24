79cd5ad146801c579d7b9b1a6c3605f0db8ab64f
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-03-24 09:27:28 +0200

    checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
         """
         Serialize _objects and saves it to a json file.
         """
-        with open(FileStorage.__file_path, "a", encoding="utf-8") as json_f:
+        with open(FileStorage.__file_path, "r+", encoding="utf-8") as json_f:
             my_dict = {k: v.to_dict()for k, v in FileStorage.__objects.items()}
             return json.dump(my_dict, json_f)
 
@@ -67,7 +67,7 @@ class FileStorage:
         if not os.path.exists(FileStorage.__file_path):
             return
 
-        with open(FileStorage.__file_path, encoding="utf-8") as json_f:
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as json_f:
             serialized_data = json.load(json_f)
         objects = {}
         for id, data in serialized_data.items():
